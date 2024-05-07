5d5854e5d5a39736236a44caa3d287d4650f94c9
Yongxin Song <yosong@student.ethz.ch>
2024-05-08 01:44:29 +0200

    bootstrappping works

--- qplib/_processor.py
+++ qplib/_processor.py
@@ -19,16 +19,20 @@ class QPTDataProcessor:
         self.folders_in = folders_in
         self.folder_out = folder_out
         self.identifier = identifier
-        self.files = [[fo_in + '/' + f + '/' + f + '.hdf5' for f in os.listdir(fo_in) if self.identifier in f]
-                      for fo_in in self.folders_in]
+        self.files = [
+            [fo_in + '/' + f + '/' + f + '.hdf5' for f in os.listdir(fo_in) if
+             self.identifier in f]
+            for fo_in in self.folders_in]
         self.files = [sorted(fo) for fo in self.files]
-        self.time_stamps = [[s.split('/')[-1][:6] for s in fo] for fo in self.files]
+        self.time_stamps = [[s.split('/')[-1][:6] for s in fo] for fo in
+                            self.files]
         self.qbs = [self.get_qb_names(fo) for fo in self.files]
         self.qb_names = [list({x for l in self.qbs[i] for x in l}) for i in
                          range(len(self.qbs))]
         self.n = n
 
-        self.pulse_periods = [[get_pulse_period(f) for f in fo] for fo in self.files]
+        self.pulse_periods = [[get_pulse_period(f) for f in fo] for fo in
+                              self.files]
         self.time_base = np.max([np.max(p) for p in self.pulse_periods])
 
         self.f = np.fft.rfftfreq(self.n, self.time_base)
@@ -48,9 +52,11 @@ class QPTDataProcessor:
         return all_qbs
 
     def set_qb_names(self, names):
-        assert len(names) == len(self.qb_names), 'names and self.qb_names must have same len'
+        assert len(names) == len(
+            self.qb_names), 'names and self.qb_names must have same len'
         for n, q in zip(names, self.qb_names):
-            assert n in q, 'qubit {} is not in the corresponding folder'.format(n)
+            assert n in q, 'qubit {} is not in the corresponding folder'.format(
+                n)
         self.names = names
 
     def get_asg_states(self, foidx, fidx, qidx):
@@ -66,7 +72,8 @@ class QPTDataProcessor:
             starts = [starts for _ in self.files]
         if type(stops) == str:
             stops = [stops for _ in self.files]
-        assert type(starts) == list and type(stops) == list, 'starts and stops must be strings or lists'
+        assert type(starts) == list and type(
+            stops) == list, 'starts and stops must be strings or lists'
         if exclude is None:
             exclude = [[] for _ in self.files]
         assert type(exclude) == list, 'exclude must be list or None'
@@ -80,18 +87,24 @@ class QPTDataProcessor:
         # i ... idx of folder, j ... idx of file, k ... idx of qubit
         for i in trange(len(self.files)):
             for j in trange(len(self.files[i])):
-                if starts[i] <= self.time_stamps[i][j] <= stops[i] and self.time_stamps[i][j] not in exclude[i]:
+                if starts[i] <= self.time_stamps[i][j] <= stops[i] and \
+                        self.time_stamps[i][j] not in exclude[i]:
                     for k in range(len(self.qbs[i][j])):
                         if self.qbs[i][j][k] == self.names[i]:
                             try:
-                                asg_states = get_exp_data_h5(self.files[i][j], channel=k)
+                                asg_states = get_exp_data_h5(self.files[i][j],
+                                                             channel=k)
                                 self.bootstrap_files.append(self.files[i][j])
                                 self.bootstrap_channels.append(k)
 
                                 jumps = get_jumps(asg_states)
 
-                                f, Pxx_den = periodogram(jumps, 1 / self.pulse_periods[i][j], scaling="density")
-                                self.bootstrap_pperiods.append(self.pulse_periods[i][j])
+                                f, Pxx_den = periodogram(jumps, 1 /
+                                                         self.pulse_periods[i][
+                                                             j],
+                                                         scaling="density")
+                                self.bootstrap_pperiods.append(
+                                    self.pulse_periods[i][j])
 
                                 # interpolate arrays to identical f grid
                                 psd = np.interp(self.f, f, Pxx_den)
@@ -117,7 +130,8 @@ class QPTDataProcessor:
         assert methode in ['mse', 'ml'], "methode not supported, use ml or mse"
 
         if filter_window is not None:
-            self.filtered_psd = savgol_filter(self.psd, filter_window, filter_poly)
+            self.filtered_psd = savgol_filter(self.psd, filter_window,
+                                              filter_poly)
         else:
             self.filtered_psd = self.psd
 
@@ -129,7 +143,9 @@ class QPTDataProcessor:
 
         if methode == 'mse':
             self.res = curve_fit(
-                lambda x, a, b, c, d, e, f: np.log10(psd_ffunc_lonly(freq=x, A=a, B=b, C=c, co_0=d, co_1=e, co_2=f)),
+                lambda x, a, b, c, d, e, f: np.log10(
+                    psd_ffunc_lonly(freq=x, A=a, B=b, C=c, co_0=d, co_1=e,
+                                    co_2=f)),
                 self.f[1:],
                 np.log10(self.psd[1:]),
                 p0=p0,
@@ -142,7 +158,8 @@ class QPTDataProcessor:
                 model = psd_ffunc_lonly(x, *params)
                 output = np.sum(np.log(model) + y / model)
 
-                if not np.isfinite(output) or np.any(params < lb) or np.any(params > ub):
+                if not np.isfinite(output) or np.any(params < lb) or np.any(
+                        params > ub):
                     return 1.0e30
                 return output
 
@@ -172,7 +189,10 @@ class QPTDataProcessor:
 
     def print_results(self):
         for i in range(3):
-            print('{}. Lorentzian component: {}, {} Hz'.format(i + 1, self.popt[i], self.popt[i + 3]))
+            print('{}. Lorentzian component: {}, {} Hz'.format(i + 1,
+                                                               self.popt[i],
+                                                               self.popt[
+                                                                   i + 3]))
 
     def save(self):
         pass  # TODO
@@ -181,13 +201,15 @@ class QPTDataProcessor:
     # BOOTSTRAPPING
     # -------------------------------------------------
 
-    @staticmethod
-    def _fit_one_psd(f, psd,
-                     p0=[0.05, 0.05, 0.05, 1e2, 1e4, 1e6],
-                     filter_window=None,
-                     filter_poly=1,
-                     lb=[0., 0., 0., 1., 1., 1.],
-                     ub=[1., 1., 1., 1e7, 1e7, 1e7],
+    # @staticmethod
+    def _fit_one_psd(self,
+                     f,
+                     psd,
+                     p0,
+                     filter_window,
+                     filter_poly,
+                     lb,
+                     ub,
                      ):
 
         if filter_window is not None:
@@ -196,7 +218,8 @@ class QPTDataProcessor:
             filtered_psd = psd
 
         res = curve_fit(
-            lambda x, a, b, c, d, e, f: np.log10(psd_ffunc_lonly(freq=x, A=a, B=b, C=c, co_0=d, co_1=e, co_2=f)),
+            lambda x, a, b, c, d, e, f: np.log10(
+                psd_ffunc_lonly(freq=x, A=a, B=b, C=c, co_0=d, co_1=e, co_2=f)),
             f[1:],
             np.log10(filtered_psd[1:]),
             p0=p0,
@@ -212,7 +235,7 @@ class QPTDataProcessor:
 
         self.popt_bootstrap = []
 
-        for i in range(0, len(self.bootstrap_files), chunksize):
+        for i in trange(0, len(self.bootstrap_files), chunksize):
 
             psd = np.zeros(self.f.shape)
             counter = 0
@@ -234,11 +257,29 @@ class QPTDataProcessor:
 
             psd /= counter
 
-            popt = self._fit_one_psd(self.f, psd,
-                                    p0=self.popt if use_popt else self.p0,
-                                    filter_window=self.filter_window,
-                                    filter_poly=self.filter_poly,
-                                    lb=self.lb,
-                                    ub=self.ub,
-                                    )
+            p0 = self.popt if use_popt else self.p0
+            popt = self._fit_one_psd(f=self.f,
+                                     psd=psd,
+                                     p0=p0,
+                                     filter_window=self.filter_window,
+                                     filter_poly=self.filter_poly,
+                                     lb=self.lb,
+                                     ub=self.ub,
+                                     )
             self.popt_bootstrap.append(popt)
+        self.popt_bootstrap = np.array(self.popt_bootstrap)
+
+    def print_stats(self):
+        for i in range(3):
+            print('Amp comp {}, '
+                  'mean {}, '
+                  'std {}, '
+                  'mean std {}'.format(i+1, np.mean(self.popt_bootstrap[:, i]),
+                                       np.std(self.popt_bootstrap[:, i]),
+                                       np.std(self.popt_bootstrap[:,
+                                              i])/np.sqrt(len(self.popt_bootstrap))))
+            print('Freq comp {}, mean {}, std {}, mean std {}'.format(i+1,
+                                                                      np.mean(self.popt_bootstrap[:, i+3]),
+                                       np.std(self.popt_bootstrap[:, i+3]),
+                                       np.std(self.popt_bootstrap[:,
+                                              i+3])/np.sqrt(len(self.popt_bootstrap))))
